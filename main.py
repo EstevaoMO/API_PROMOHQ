fdbc3ffc94649b49441aff958fe52ba63f78f92a
EstevaoMO <161775124+EstevaoMO@users.noreply.github.com>
2025-08-18 22:59:47 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+import json
+from starlette.responses import JSONResponse
+from services import scrape_panini
+
+num_pag = 1 # Página padrão para iniciar
+urls_dc = {
+    "batman": f"https://panini.com.br/dc-comics/universo-batman?p={num_pag}&price=14-105&product_list_order=high_to_low",
+    "superman": f"https://panini.com.br/dc-comics/universo-superman?p={num_pag}&price=14-105&product_list_order=high_to_low",
+    "mulher-maravilha": f"https://panini.com.br/dc-comics/universo-mulher-maravilha?p={num_pag}&price=14-105&product_list_order=high_to_low",
+    "liga-da-justica": f"https://panini.com.br/dc-comics/universo-liga-da-jusica?p={num_pag}&price=14-105&product_list_order=high_to_low",
+    "flash": f"https://panini.com.br/dc-comics/universo-flash?p={num_pag}&price=14-105&product_list_order=high_to_low"
+}
+
+urls_marvel = {
+    "quarteto-fantastico": f"https://panini.com.br/marvel/universo-quarteto-fantastico?p={num_pag}&price=13-199&product_list_order=high_to_low",
+    "homem-aranha": f"https://panini.com.br/marvel/universo-homem-aranha?p={num_pag}&price=13-174&product_list_order=high_to_low"
+}
+
+modelo_saida_classes = {
+    "link_produto": "product photo product-item-photo", # a ["href"]
+    "nome_produto": "product-item-link", # a .text
+    "imagem_produto": "product-image-photo", # Filtrar img ["data-srcset"]
+    "preco_antigo": "old-price", # span .text
+    "preco_atual": "price" # span .text
+}
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Bem-vindo ao PROMO HQ"
+
+@app.get("/dc-tudo")
+def buscar_tudo_dc():
+    dc_data = scrape_panini(urls_dc, modelo_saida_classes)
+    json.dumps(dc_data, indent=4, ensure_ascii=False)
+    return dc_data
+
+@app.get("/marvel-tudo")
+def buscar_tudo_marvel():
+    marvel_data = scrape_panini(urls_marvel, modelo_saida_classes)
+    json.dumps(marvel_data, indent=4, ensure_ascii=False)
+    return marvel_data
---
+++ services.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def scrape_panini(urls, modelo_de_classes): 
+    todos_universos = {}
+    for universo, url_universoglobal in urls.items():
+        dados_universo = []
+
+        for num_pag in range(1, 11):  # Busca apenas nas 10 primeiras páginas
+            url = url_universoglobal.format(num_pag=num_pag) # Formata para usar a página atual
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            hqs = soup.find_all('li', class_='item product product-item')
+
+            for hq in hqs:
+                hq_info = {}
+
+                # Pegando link
+                link_tag = hq.find('a', class_=modelo_de_classes['link_produto'])
+                if link_tag:
+                    hq_info['link_produto'] = link_tag['href']
+
+                # Pegando nome
+                name_tag = hq.find('a', class_=modelo_de_classes['nome_produto'])
+                if name_tag:
+                    hq_info['nome_produto'] = name_tag.text
+
+                # Pegando imagem
+                img_tag = hq.find('img', class_=modelo_de_classes['imagem_produto'])
+                if img_tag and 'data-srcset' in img_tag.attrs:
+                    # Tratando o dataset de links
+                    image_urls = img_tag['data-srcset'].split(',')
+                    if image_urls:
+                        hq_info['imagem_produto'] = image_urls[-1].strip().split(' ')[0] # Pega o último link do dataset
+
+                # Pegando preço antigo
+                preco_antigo_tag = hq.find('span', class_=modelo_de_classes['preco_antigo'])
+                if preco_antigo_tag:
+                    hq_info['preco_antigo'] = preco_antigo_tag.text.strip()
+
+                # Pegando preço atual
+                preco_atual_tag = hq.find('span', class_=modelo_de_classes['preco_atual'])
+                if preco_atual_tag:
+                    hq_info['preco_atual'] = preco_atual_tag.text.strip()
+
+                if hq_info: # Só adiciona se realmente houve dados extraídos
+                    dados_universo.append(hq_info)
+
+        todos_universos[universo] = dados_universo
+    
+    return todos_universos
